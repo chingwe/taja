f153ab079a9c155e1f16a3f1e3689fd1461d9e92
chingwe <zhangjingwen137@gmail.com>
2022-10-25 21:16:29 +0800

    Initial Commit

---
+++ taja/tajaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tajaApp.swift
+//  taja
+//
+//  Created by 張靜文 on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct tajaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
